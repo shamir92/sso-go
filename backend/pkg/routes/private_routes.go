2e2d5e85357e20a92694e58c85750709cb04b6fb
Shamir Husein <shamir.husein@justdelegate.co>
2022-04-24 17:26:11 +0700

    add

---
+++ backend/pkg/routes/private_routes.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"sso-go/app/controllers"
+sso-go
+	"sso-go/pkg/middleware"
+	"github.com/gofiber/fiber/v2"
+)
+
+// PrivateRoutes func for describe group of private routes.
+func PrivateRoutes(a *fiber.App) {
+	// Create routes group.
+	route := a.Group("/api/v1")
+
+	// Routes for POST method:
+	route.Post("/book", middleware.JWTProtected(), controllers.CreateBook)           // create a new book
+	route.Post("/user/sign/out", middleware.JWTProtected(), controllers.UserSignOut) // de-authorization user
+	route.Post("/token/renew", middleware.JWTProtected(), controllers.RenewTokens)   // renew Access & Refresh tokens
+
+	// Routes for PUT method:
+	route.Put("/book", middleware.JWTProtected(), controllers.UpdateBook) // update one book by ID
+
+	// Routes for DELETE method:
+	route.Delete("/book", middleware.JWTProtected(), controllers.DeleteBook) // delete one book by ID
+}
